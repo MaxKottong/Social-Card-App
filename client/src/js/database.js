fe939b04c19a623da01436100e12a5650c797c8f
Max Kottong <max.kottong@gmail.com>
2022-10-05 12:11:34 -0400

    Added delete and add functionality, as well as database changes and card retreival upon delete

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,5 +12,52 @@ export const initdb = async () => {
             db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
             console.log('contacts store created');
         }
-    })
-}
+    });
+};
+
+export const getDb = async () => {
+    console.log('GET from the database');
+
+    const contactDb = await openDB('contact_db', 1);
+
+    const tx = contactDb.transaction('contacts', 'readonly');
+
+    const store = tx.objectStore('contacts');
+
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database');
+
+    const contactDb = await openDB('contact_db', 1);
+
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    const store = tx.objectStore('contacts');
+
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+
+    const result = await request;
+    console.log('Data saved to the database', result);
+};
+
+export const deleteDb = async (id) => {
+    console.log('DELETE from the database', id);
+
+    const contactDb = await openDB('contact_db', 1);
+
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    const store = tx.objectStore('contacts');
+
+    const request = store.delete(id);
+
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,4 @@
 import "./form";
-import "./submit";
 
 import "../css/index.css";
 
@@ -10,11 +9,52 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
-import { initdb } from './database';
+import { initdb, getDb, postDb, deleteDb } from './database';
+
+import { fetchCards } from './cards';
+
+import { toggleForm, clearForm } from './form';
 
 window.addEventListener('load', function () {
     initdb();
+    fetchCards();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 });
+
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+    toggleForm()
+})
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    if (submitBtnToUpdate == false) {
+        postDb(name, email, phone, profile);
+    } else {
+
+        fetchCards();
+        submitBtnToUpdate = false;
+    }
+
+    clearForm();
+    toggleForm();
+    fetchCards();
+});
+
+window.deleteCard = (e) => {
+    let id = parseInt(e.id);
+
+    deleteDb(id);
+    fetchCards();
+};
